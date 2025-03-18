0056cb19c451e0168105c346d781a27c156fbda0
DamjanJaklinPolic <103748907+DamjanJaklinPolic@users.noreply.github.com>
2025-03-18 11:19:36 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const menu = document.getElementById("navMenu");
+  const hamburger = document.querySelector(".hamburger");
+  menu.classList.toggle("open");
+  hamburger.classList.toggle("open");
+}
